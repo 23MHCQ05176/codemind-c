96704fea55408e78e9fab33babf1a27222c994cc
23MHCQ05176 <147487693+23MHCQ05176@users.noreply.github.com>
2024-01-03 13:26:15 +0530

    #include<stdio.h> int main() {     int a,i,j;     scanf("%d",&a);     for(i=1;i<=a;i++)     {         for(j=a;j>=1;j--)         {             if(i>=j)             {                 printf("* ");             }             else             {                 printf(" ");             }         }         printf(" ");     } }

---
+++ Pattern_6.1.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+    int a,i,j;
+    scanf("%d",&a);
+    for(i=1;i<=a;i++)
+    {
+        for(j=a;j>=1;j--)
+        {
+            if(i>=j)
+            {
+                printf("* ");
+            }
+            else
+            {
+                printf(" ");
+            }
+        }
+        printf("
+");
+    }
+}
